4b78ae5cea78e781f30d05261d41620ebe00bbbe
secondman <secondman@jetstreamlabs.com>
2025-07-16 22:49:18 -0700

    chore: welcome page typos
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -18,12 +18,12 @@ const appName = computed(() => page.props.name);
       <div class="mb-12">
         <h2 class="mb-4 text-xl font-medium text-slate-400 md:text-2xl">{{ __('Coming Soon') }}</h2>
         <h1 class="mb-4 text-2xl font-semibold text-white md:text-4xl">{{ appName }}</h1>
-        <p class="text-lg text-slate-400 md:text-xl">{{ __("We're working hard to bring you something amazing. Stay tuned!") }}'</p>
+        <p class="text-lg text-slate-400 md:text-xl">{{ __("We're working hard to bring you something amazing. Stay tuned!") }}</p>
       </div>
 
       <!-- Optional Contact Info -->
       <div class="text-sm text-slate-400">
-        <p>{{ __('For inquiries, please contact us at:') }}'</p>
+        <p>{{ __('For inquiries, please contact us at:') }}</p>
         <a href="mailto:hello@jetstreamlabs.com" class="font-medium text-orange-400 hover:text-orange-600">
           {{ __('hello@jetstreamlabs.com') }}
         </a>
